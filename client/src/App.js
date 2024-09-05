2f35108953c987bd65c6155f5b89d49ddd7b728f
rajnish kumar <rajnishshk@gmail.com>
2024-09-06 01:05:36 +0530

    backend completed and frontend started

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React, { Fragment} from 'react';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+// import { Switch } from 'react-router-dom';
+
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+import './App.css';
+
+const App = ()=> (
+  <Router>
+    <Fragment>
+    <Navbar />
+  <Routes>
+    <Route path="/" element={<Landing />} />
+  </Routes> 
+<section className="components">
+  <Routes>
+    <Route path="/register" element={<Register />} />
+    <Route path="/login" element={<Login />} />
+  </Routes>
+</section>
+    </Fragment>
+  </Router>
+    
+);
+
+export default App;
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -144,5 +144,73 @@ router.put("/unlike/:id", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+//---------------------------------------------------
+//@route             POST api/posts/comment/:id
+//@description       comment on a post
+//@access            private
+
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "Text is Required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+      const post = await Post.findById(req.params.id);
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+      post.comments.unshift(newComment);
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+//---------------------------------------------------
+//@route             Delete api/posts/comment/:id/:comment_id
+//@description       Delete a comment
+//@access            private
+
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    // Pull out comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    // Make Sure comment Exists
+    if (!comment) {
+      res.status(404).json({ msg: "comment does not exist.." });
+    }
+
+    // check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not Authorized" });
+    }
+    // Get Remove Index
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
